d30d5a08a995da157e897fec39aa4daa07b3ca7c
Travis Baker <travis.baker@papercut.com>
2020-05-25 20:25:06 +1000

    Updates

---
+++ precache-manifest.2e37d5958458753ad67bde8283446047.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783060fbc78cb6a7ffe05ac2c239edec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5c9eb4c4ddc7bb132785",
+    "url": "/monsters-rolodex/static/css/main.35f29830.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9eb4c4ddc7bb132785",
+    "url": "/monsters-rolodex/static/js/main.bab089d1.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
